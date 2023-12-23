758bbc7138c4ede112c59dc97b2cb2c290d549ff
anandc <anand.chaudhary@datapoem.com>
2023-12-23 18:36:17 +0530

    basic auth and resource server

---
+++ resourceServerOauth/src/main/java/com/example/resourceServerOauth/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.resourceServerOauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->{
+            auth.anyRequest().authenticated();
+        }).oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
